5f8564fe6fcfe992dfa17d9f72b3c916635d9a83
John Cobbs <johnjosephcobbs@gmail.com>
2021-08-14 10:45:57 -0700

    update

--- script.js
+++ script.js
@@ -10,7 +10,16 @@ function writePassword() {
 
 }
 function generatePassword(){
+  var lenth = 12
+  for (i = 0; i < length; i++) {
   return String.fromCharCode(Math.floor(Math.random() * 26) +97);
 }
+
+// function generatePassword(){
+//   var characters = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()[]\{}|:'<>?'";
+//   var length = 12;
+//   for (i = 0; i < length; i++) {
+//     return String.fromCharCode(Math.floor(Math.random() * 26) +97);
+// }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
